a1fec6d16e44a7e40e57a06948f1686453cab774
mutek <mutek@riseup.net>
2013-07-01 15:57:43 +0000

    inizializzazione

---
+++ App/ioTorBundle2.3.25.10-Debian7/Linux-ia32/bin/App/Firefox/defaults/pref/channel-prefs.js
@@ -0,0 +1,6 @@
+//@line 2 "/srv/build-trees/build-alpha/i686/firefox-17.0.7esr/browser/app/profile/channel-prefs.js"
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+pref("app.update.channel", "default");
